eb0cc0c9e4cf2d115b6004d44e4c8bfe51696730
RetaxMaster <retaxmaster@hotmail.com>
2019-12-06 03:19:32 -0600

    Modificacion de las librerias 1

--- phplib/tracks.php
+++ phplib/tracks.php
@@ -1,29 +1,30 @@
 <?php
 /*	
- tbl_acadcaps
-	id_cap
+ tbl_crmtracks
+	id_track
+    id_cclient
+    id_client
     id_user
-    formacad
-    skills
-    certif
-    exp
-    otros
+    id_proc
+    id_subproc
+    descrip
+    fcontact
 */
 
 // Crea una nueva ubicación
-function caps_createRecord($id_user, $formacad, $skills, $certif, $exp, $otros)
+function track_createRecord($id_cclient, $id_client, $id_user, $id_proc, $id_subproc, $descrip, $fcontact)
 {
-    $SQLStrQuery = "CALL sp_p_set_acadcaps_Create('$id_user', '$formacad', '$skills', '$certif', '$exp', '$otros')";
+    $SQLStrQuery = "CALL sp_p_set_crmtrack_Create('$id_cclient', '$id_client', '$id_user', '$id_proc', '$id_subproc', '$descrip', '$fcontact')";
     SQLQuery($ResponsePointer, $n, $SQLStrQuery, false); // Realiza la consulta
 }
 
 // Actualiza algún campo de la tabla según su id, recibe un arreglo asociativo con los campos a actualizar y también recibe el id de la fila que se va a actualizar
-function caps_updateRecord($fields, $id_cap)
+function track_updateRecord($fields, $id_informin)
 {
     if (!empty($fields)) {
         foreach ($fields as $key => $value) {
             if ($value !== "") {
-                $SQLStrQuery = "CALL sp_p_set_acadcaps_Update('$key', '$value', $id_cap)";
+                $SQLStrQuery = "CALL sp_p_set_crmtrack_Update('$key', '$value', $id_informin)";
                 SQLQuery($ResponsePointer, $n, $SQLStrQuery, false); // Realiza la consulta
             }
         }
@@ -33,28 +34,28 @@ function caps_updateRecord($fields, $id_cap)
 }
 
 //Recupera todos los registros, opcionalmente puedes especificar si deseas hacer un join para traer los datos crudos o reemplazados
-function caps_recoveryAllList(&$nDocs, &$Docs, $join = false)
+function track_recoveryAllList(&$nDocs, &$Docs, $join = false)
 { // true or false
     $tinyint = (int) $join;
-    $SQLStrQuery = "CALL sp_p_lst_acadcaps_all($tinyint)";
+    $SQLStrQuery = "CALL sp_p_lst_crmtrack_all($tinyint)";
     SQLQuery($ResponsePointer, $nDocs, $SQLStrQuery, true); // Realiza la consulta
     ConvertPointerToArray($ResponsePointer, $Docs, $nDocs, 8); // Pertenece a dbmngmtAdmin.php
 }
 
 //Recupera todos los registros filtrados por algún campo
-function caps_recoveryAllByAnyField(&$nDocs, &$Docs, $field, $value, $extraWhere, $join = false)
+function track_recoveryAllByAnyField(&$nDocs, &$Docs, $field, $value, $extraWhere, $join = false)
 { // true or false
     $tinyint = (int) $join;
-    $SQLStrQuery = "CALL sp_p_lst_acadcaps_byAnyField('$field', '$value', $tinyint, '$extraWhere')";
+    $SQLStrQuery = "CALL sp_p_lst_crmtrack_byAnyField('$field', '$value', $tinyint, '$extraWhere')";
     SQLQuery($ResponsePointer, $nDocs, $SQLStrQuery, true); // Realiza la consulta
     ConvertPointerToArray($ResponsePointer, $Docs, $nDocs, 8); // Pertenece a dbmngmtAdmin.php
 }
 
 //Recupera un registro filtrados por algún campo
-function caps_recoveryOneByAnyField(&$nDocs, &$Docs, $field, $value, $extraWhere, $join = false)
+function track_recoveryOneByAnyField(&$nDocs, &$Docs, $field, $value, $extraWhere, $join = false)
 { // true or false
     $tinyint = (int) $join;
-    $SQLStrQuery = "CALL sp_p_get_acadcaps_byAnyField('$field', '$value', $tinyint, '$extraWhere')";
+    $SQLStrQuery = "CALL sp_p_get_crmtrack_byAnyField('$field', '$value', $tinyint, '$extraWhere')";
     SQLQuery($ResponsePointer, $nDocs, $SQLStrQuery, true); // Realiza la consulta
-    ConvertPointerToArray($ResponsePointer, $Docs, $nDocs, 7); // Pertenece a dbmngmtAdmin.php
+    ConvertPointerToArray($ResponsePointer, $Docs, $nDocs, 8); // Pertenece a dbmngmtAdmin.php
 }
